0254b602cfbd82cdfce164cb85d04f2fab4289f9
ianchu <ianchu32123@gmail.com>
2024-05-14 01:25:30 +0800

    5/14

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,17 @@
 import React from "react";
-
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 export default function App() {
-  return <p>App</p>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>App</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 }
