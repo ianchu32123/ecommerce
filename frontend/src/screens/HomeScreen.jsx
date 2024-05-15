930287da2fe02297f15b6b26d3efcbdb3bfb3b8e
ianchu <ianchu32123@gmail.com>
2024-05-15 20:39:33 +0800

    使用redux toolkit 做前後端資料串接

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,28 +1,31 @@
+import React from "react";
 import { Row, Col } from "react-bootstrap";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useGetProductsQuery } from "../slices/productApiSlice";
 import Product from "../components/Product";
 
 export const HomeScreen = () => {
-  const [Products, setProduct] = useState([]);
+  const { data: products, isLoading, isError, error } = useGetProductsQuery();
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      setProduct(data);
-    };
-    fetchProducts();
-  }, []);
   return (
     <>
-      <h1>最新產品</h1>
-      <Row>
-        {Products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : isError ? (
+        <h2>{error?.data?.message || error.error}</h2>
+      ) : (
+        <>
+          <h1>最新產品</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
+
+export default HomeScreen;
