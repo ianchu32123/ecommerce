74f06f94fbc27a42e1bea13d4fc245d073790a9a
ianchu <ianchu32123@gmail.com>
2024-05-16 17:03:37 +0800

    jwt設置

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
-
+import jwt from "jsonwebtoken";
 //@desc 登入判斷
 //@route Post /api/users/login
 //@access Public
@@ -10,11 +10,22 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+    //設定cookie
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000, //30天
+    });
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
+      //   token: generateToken(user._id),
     });
   } else {
     res.status(401);
